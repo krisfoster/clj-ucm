5b1d498902c3f5b27c3d8bfdf7368b0a0e64b280
Kris <kristian.foster@gmail.com>
2011-01-31 16:43:45 +0000

    Checked in leiningen project

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject clj-ucm "1.0.0-SNAPSHOT"
+  :description "Simple library wrapping around the Oracle RIDC client library fo UCM.
+                This has been developed and tested against UCM 10Gr3 - may work with the new version but haven't tried it yet.
+                The one Oracle library is not available through clojars - or jarvana
+                so it needs to be added to you local maven repository as below:
+
+                  $ mvn install:install-file -Dfile=./ridc/oracle-ridc-client-10g.jar -DgroupId=oracle -DartifactId=ridc -Dversion=10.0 -Dpackaging=jar -DgeneratePom=true
+
+               The above bit of script requires that the jar is downloaded and placed in the named directories. They 
+               can be downloaded from - accept the the license agreement and click on the link to download Content Integration Suite:
+
+               * http://www.oracle.com/technetwork/middleware/content-management/downloads/index-ucm10g-082682.html
+
+               "
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+                 [clj-time "0.1.0-RC1"]
+                 [oracle/ridc "10.0"]
+                 ]
+  :dev-dependencies [
+                ; Needed for vim integration
+                [vimclojure/server "2.3.0-SNAPSHOT"]
+                [org.clojars.autre/lein-vimclojure "1.0.0"]]
+                ;
+  )
+
---
+++ test/clj_ucm/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-ucm.test.core
+  (:use [clj-ucm.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
